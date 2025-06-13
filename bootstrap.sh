61d98dfe1cc4076543e0d33f434baa4d5041a201
Vincent Shen <wenshen@redhat.com>
2025-06-13 08:02:25 -0700

    Fix fbc on 4.17-4.19 enterprise contract test
    on and after OCP 4.17, we need to run opm render with  bundle-object-to-csv-metadata to generated expected catalog file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 set -x
-OCP_VERSIONS=4.17
-for VERSION in ${OCP_VERSIONS}
-do
+for VERSION in "${OCP_VERSIONS[@]}"; do
     CONTAINERFILE="Containerfile-rhel-9.in"
     if [[ "$VERSION" =~ ("4.12"|"4.13"|"4.14") ]]; then
             CONTAINERFILE="Containerfile-rhel-8.in"
@@ -13,7 +11,12 @@ do
     cp "${CONTAINERFILE}" "catalog/v${VERSION}/Containerfile"
     sed -i "s/OCP_VERSION/${VERSION}/g" "catalog/v${VERSION}/Containerfile"
     opm alpha convert-template basic -o yaml "./catalog-migrate-${VERSION}/security-profiles-operator/catalog.json" > "catalog/v${VERSION}/catalog-template.yaml"
-    opm alpha render-template basic -o yaml "catalog/v${VERSION}/catalog-template.yaml" > "catalog/v${VERSION}/security-profiles-operator/catalog.yaml"
+    # # --- 1) Render the new bundle into a temp file ---
+    if [[ "$VERSION" =~ ("4.12"|"4.13"|"4.14"|"4.15"|"4.16") ]]; then
+        opm alpha render-template basic -o yaml "catalog/v${VERSION}/catalog-template.yaml" > "catalog/v${VERSION}/security-profiles-operator/catalog.yaml"
+    else
+        opm alpha render-template basic -o yaml "catalog/v${VERSION}/catalog-template.yaml" --migrate-level=bundle-object-to-csv-metadata > "catalog/v${VERSION}/security-profiles-operator/catalog.yaml"
+    fi
 
     echo "Building locally to ensure it works"
     podman build -t "fio-fbc-${VERSION}" -f "catalog/v${VERSION}/Containerfile" "catalog/v${VERSION}/" && rm -rf "./catalog-migrate-${VERSION}"
--- update.sh
+++ update.sh
@@ -3,16 +3,16 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # --- CONFIGURATION ---
-OCP_VERSIONS=(4.12 4.13 4.14 4.15 4.16 4.17 4.18 4.19)
+OCP_VERSIONS=(4.13 4.14 4.15 4.16 4.17 4.18 4.19)
 
 # Old (tag-based) image:
-NEW_BUNDLE="quay.io/redhat-user-workloads/ocp-isc-tenant/security-profiles-operator-bundle@sha256:4fc37a20974c89191f0c515f4abb30b26a2f11641682e2932fefed3850ae3a2c"
+NEW_BUNDLE="quay.io/redhat-user-workloads/ocp-isc-tenant/security-profiles-operator-bundle-release:release-0-9"
 
 # New registry/repo to use, but we’ll attach the old image’s actual digest.
 REDHAT_REGISTRY_REPO="registry.redhat.io/compliance/openshift-security-profiles-operator-bundle"
 
 # (Optional) Some additional parameters you might use later
-OP_V="0.8.6"
+OP_V="0.9.0"
 CSV_NEW="security-profiles-operator.v${OP_V}"
 SKIP_RANGE=">=1.0.0 <${OP_V}"
 
@@ -39,9 +39,13 @@ for OCP_V in "${OCP_VERSIONS[@]}"; do
 
   echo "🔎 Updating ${CATALOG}…"
 
-  # # --- 1) Render the new bundle into a temp file ---
-  opm render "${NEW_BUNDLE}" --output=yaml >> "${CATALOG}"
-
+  # If an existing entry exists we should remove it so that we can generate a
+  # fresh index with updated references. If we don't take this step, then we
+  # could end up with two entries with the same release, which will fail opm
+  # validation. Here we're removing the CSV_NEW entry entirely and the
+  # relationship between the last version and the new version (e.g., CSV_NEW).
+  yq eval-all -i "select(.name? != \"${CSV_NEW}\")" "${CATALOG}"
+  yq eval -i "del(.entries[] | select(.name? == \"${CSV_NEW}\"))" "${CATALOG}"
 
   # 1) Find the "last" name in the stable channel's entries array.
   LAST_NAME=$(yq eval '
@@ -50,6 +54,14 @@ for OCP_V in "${OCP_VERSIONS[@]}"; do
   ' "${CATALOG}")
 
   echo "Last entry in stable channel is: ${LAST_NAME}"
+
+  # # --- 1) Render the new bundle into a temp file ---
+  if [[ "$OCP_V" =~ ("4.12"|"4.13"|"4.14"|"4.15"|"4.16") ]]; then
+    opm render "${NEW_BUNDLE}" --output=yaml >> "${CATALOG}"
+  else
+    opm render "${NEW_BUNDLE}" --output=yaml --migrate-level bundle-object-to-csv-metadata >> "${CATALOG}"
+  fi
+
 
   # 2) In-place update: remove any old entry named CSV_NEW, then add one new entry.
   yq eval -i -I1 "
